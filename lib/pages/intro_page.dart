8f80258091294ed61b53ec583238156b044e8200
BexTheFrog <beatriz.go.plateiro@gmail.com>
2025-08-20 17:29:52 -0300

    Pagina Introdutória

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,121 @@
+import 'package:ebbie/config/app_colors.dart';
+import 'package:ebbie/pages/homepage.dart';
+import 'package:ebbie/widgets/module_intropage/custom_btn.dart';
+import 'package:flutter/material.dart';
+import '../widgets/module_intropage/custom_form_field.dart';
+import '../widgets/module_intropage/custom_form_label.dart';
+
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyHomePage()),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16, top: 15),
+              child: Text(
+                "Continuar sem Acesso",
+                style: TextStyle(
+                  fontFamily: 'CerebriSansPro',
+                  fontSize: 18,
+                  color: AppColors.tealBlue,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+       body: 
+       Stack(
+        children: [
+
+        Positioned(
+          top: 325,       
+          right: 45,    
+          width: 700,   
+          child: Image.asset(
+            'assets/images/pedaco.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        Column(
+          children: [
+
+            SizedBox(height: 45),
+
+            Center(
+              child: Image.asset('assets/images/logo_gradient.png', width: 300,)
+            ),
+
+            SizedBox(height: 35),
+
+            Container(
+              padding: EdgeInsets.only(left:10, top:0, right: 10, bottom: 0),
+              alignment: Alignment.topCenter,
+              width: 330,
+              child: Text("Bem vindo ao Ebbie, seu novo planner estudantil", 
+              style: TextStyle(
+                fontFamily: 'CerebriSansPro', 
+                fontSize: 24,
+                color: AppColors.tealBlue ),)),
+
+            SizedBox(height: 25),
+
+            Form(
+              child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 0, 25, 5),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      LabelsForm(title: 'Email:'),
+                      SizedBox(height: 5),
+                      CustomFormField(hintText: 'Usuario@mail.com', state: false,),
+                      SizedBox(height: 20,),
+                      LabelsForm(title: 'Senha:'),
+                      SizedBox(height: 5),
+                      CustomFormField(hintText: 'Senha', state: true,),
+                      SizedBox(height: 40,),
+                      Center(child: BtnForm(title: 'Acessar', cor: AppColors.darkSlate,)),
+                      SizedBox(height: 15,),
+                    ],
+                  ),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BtnForm(title: 'Criar Conta', cor: AppColors.tealBlue,),
+                    SizedBox(width: 30,),
+                    BtnForm(title: 'Esqueci a Senha', cor: AppColors.tealBlue,),
+                  ],
+                )
+              ],
+            )),
+          ],
+        )
+      ],
+      )
+    );
+  }
+}
+
