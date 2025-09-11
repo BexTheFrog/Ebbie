afd7d55af33c18146669f62b5113fd9227b1098c
BexTheFrog <beatriz.go.plateiro@gmail.com>
2025-09-11 16:01:23 -0300

    Alterações do navbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ebbie/pages/homepage.dart';
+import 'package:ebbie/pages/intro_page.dart';
 import 'package:ebbie/pages/pomodoro_page.dart';
+import 'package:ebbie/pages/profile_page.dart';
 import 'package:ebbie/pages/profile_page.dart';
 import 'package:ebbie/pages/revision_page.dart';
 import 'widgets/bottom_nav.dart';
@@ -13,7 +15,7 @@ class Ebbie extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-            home: ProfilePage(),
+      home: const MyHomePage(),
     );
   }
 }
